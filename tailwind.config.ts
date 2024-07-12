bdc2371e151a58f24196a7f9af4ab1243cf45ce9
The Ace <udonsichisom02@gmail.com>
2024-07-12 20:12:34 +0100

    feat(app): initialized next app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#1db954",
+        neutral: {
+          100: "#ffffff",
+          200: "#b3b3b3",
+          300: "#535353",
+          800: "#212121",
+          900: "#121212",
+        },
       },
     },
   },
